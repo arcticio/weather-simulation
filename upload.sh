62fe51d94d1a6f62524ef6fe8501d17d776af435
arcticio <tb@arctic.io>
2017-06-14 12:48:50 +0200

    prep GAE deployment

--- upload.sh
+++ upload.sh
@@ -1,4 +1,7 @@
-###
+#!/bin/bash
 
 
-google_appengine/appcfg.py update . --email=noiv11@gmail.com 
+## https://cloud.google.com/appengine/docs/standard/python/tools/appcfg-arguments
+
+
+python2.7 /home/noiv/Programs/google_appengine/appcfg.py update . --email=noiv11@gmail.com 
